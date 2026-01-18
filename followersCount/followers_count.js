3e537f09dac6a5c60c8026ec7340fd9c9b835fd0
Your Name <you@example.com>
2026-01-18 15:03:45 -0500

    Added a reset counter button

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -6,6 +6,12 @@ function increaseCount() {
   checkCountValue(); // Check count value and display messages
 }
 
+function resetCount() {
+  count=0; // Increment the count by 1
+  displayCount(); // Display the count
+  alert("Followers count has been reset!");
+}
+
 function displayCount() {
 document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
 }
